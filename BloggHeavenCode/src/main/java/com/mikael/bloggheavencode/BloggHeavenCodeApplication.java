0eea9c0de2bd5e2685a4407f5ff5c74475b9e9b1
Mikael Eriksson <m.eriksson@gmx.com>
2023-06-17 20:20:55 +0200

    Create the project, with dependencies that are good for a start of a restful app. Database settings are also in the application.properties, so there is connection to a backend.

---
+++ BloggHeavenCode/src/main/java/com/mikael/bloggheavencode/BloggHeavenCodeApplication.java
@@ -0,0 +1,13 @@
+package com.mikael.bloggheavencode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggHeavenCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggHeavenCodeApplication.class, args);
+    }
+
+}
---
+++ BloggHeavenCode/src/test/java/com/mikael/bloggheavencode/BloggHeavenCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikael.bloggheavencode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggHeavenCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
