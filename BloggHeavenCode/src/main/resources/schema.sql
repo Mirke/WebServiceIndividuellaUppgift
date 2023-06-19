392d2d8436770c0f9d88ac2b2664bfb80f5d4741
Mikael Eriksson <m.eriksson@gmx.com>
2023-06-19 13:10:53 +0200

    schema.sql for setting up the tables in the database

---
+++ BloggHeavenCode/src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+USE bloggheaven;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS address;
+
+CREATE TABLE user
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(90) NOT NULL,
+    lastName VARCHAR(90) NOT NULL,
+    address VARCHAR(90) NOT NULL,
+    email VARCHAR(90) NOT NULL,
+    phone VARCHAR(90) NOT NULL,
+    memberTypes VARCHAR(90) NOT NULL
+);
+
+CREATE TABLE address
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    street VARCHAR(90) NOT NULL,
+    postalCode VARCHAR(90) NOT NULL,
+    city VARCHAR(90) NOT NULL,
+    country VARCHAR(90) NOT NULL
+);
